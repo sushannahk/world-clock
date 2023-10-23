1a6ce0af21c18cb485821e1811a9682734c79664
Zuzanna Kuczyńska-Cisowska <zuza.kuczynska@gmail.com>
2023-10-23 17:47:13 +0200

    Added function to handle changes in city select

--- src/index.js
+++ src/index.js
@@ -22,5 +22,29 @@ function updateTime() {
 	)} ${londonTime.format("[<small>]A[</small>]")}  `;
 }
 
+function updateCity(event) {
+	let cityTimeZone = event.target.value;
+	let cityTime = moment().tz(cityTimeZone);
+	let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+	let citiesElement = document.querySelector("#cities");
+	citiesElement.innerHTML = `
+            <div class="city">
+                <div>
+                    <h2>${cityName}</h2>
+                    <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+                </div>
+
+                <div class="time">${cityTime.format("h:mm:ss")} 
+                                <small>
+								 ${cityTime.format("A")} 
+                                 </small>
+                </div>
+            </div>
+    `;
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
