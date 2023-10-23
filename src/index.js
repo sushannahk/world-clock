0268de490c88df159fb4a22455475a5927706dc3
Zuzanna Kuczyńska-Cisowska <zuza.kuczynska@gmail.com>
2023-10-23 15:53:40 +0200

    Added  current date and time of a location

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
+function updateTime() {
+	// Paris
+	parisElement = document.querySelector("#paris");
+	parisDateElement = parisElement.querySelector(".date");
+	parisTimeElement = parisElement.querySelector(".time");
+	parisTime = moment().tz("Europe/Paris");
 
+	parisDateElement.innerHTML = moment().format("MMMM Do YYYY");
+	parisTimeElement.innerHTML = `${parisTime.format(
+		"h:mm:ss"
+	)} ${parisTime.format("[<small>]A[</small>]")}  `;
+
+	// London
+	londonElement = document.querySelector("#london");
+	londonDateElement = londonElement.querySelector(".date");
+	londonTimeElement = londonElement.querySelector(".time");
+	londonTime = moment().tz("Europe/London");
+
+	londonDateElement.innerHTML = moment().format("MMMM Do YYYY");
+	londonTimeElement.innerHTML = `${londonTime.format(
+		"h:mm:ss"
+	)} ${londonTime.format("[<small>]A[</small>]")}  `;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
